3a68d454cfbcc40e25d1aaf3b963b61b88173214
JoseLLoredoHdz <lluisloredo558@gmail.com>
2025-08-15 17:48:15 -0600

    Agrege a funcion posicionCarrusel() para poder moverse con puntos ya con eso poder mover la imagen entendi el DOM y agregue mas imagenes para poner en la pagina son imagenes de gelatinas

--- app.js
+++ app.js
@@ -24,9 +24,12 @@ let atras = document.getElementById('atras');
 let adelante = document.getElementById('adelante');
 let imagen = document.getElementById('img');
 let puntos = document.getElementById('puntos');
-let texto = document.getElementById('texto')
-let actual = 0
+let texto = document.getElementById('texto');
+
+let actual = 0;
 posicionCarrusel()
+
+
 
 atras.addEventListener('click', function(){
     actual -=1
@@ -59,12 +62,55 @@ adelante.addEventListener('click', function(){
 
 function posicionCarrusel() {
     puntos.innerHTML = ""
-    for (var i = 0; i <imagenes.length; i++){
+    for (let i = 0; i <imagenes.length; i++){
         if(i == actual){
-            puntos.innerHTML += '<p class="bold">.<p>'
+            puntos.innerHTML += `<p class="bold" id="punto${i}">.<p>`
         }
         else{
-            puntos.innerHTML += '<p>.<p>'
+            puntos.innerHTML += `<p id="punto${i}">.<p>`
         }
     } 
+
+        
+    let punto1 = document.getElementById('punto0');
+    let punto2 = document.getElementById('punto1');
+    let punto3 = document.getElementById('punto2');
+
+        
+    punto1.addEventListener('click',()=>{
+       
+        actual = 0;
+        imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+        texto.innerHTML = `
+        <h3>${imagenes[actual].nombre}</h3>
+        <p>${imagenes[actual].descripcion}</p>
+        `
+        posicionCarrusel()
+    })
+
+    punto2.addEventListener('click',()=>{
+        
+        actual = 1;
+        imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+        texto.innerHTML = `
+        <h3>${imagenes[actual].nombre}</h3>
+        <p>${imagenes[actual].descripcion}</p>
+        `
+        posicionCarrusel()
+    })
+
+    punto3.addEventListener('click',()=>{
+        
+        actual = 2;
+        imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+        texto.innerHTML = `
+        <h3>${imagenes[actual].nombre}</h3>
+        <p>${imagenes[actual].descripcion}</p>
+        `
+        posicionCarrusel()
+    })
+
+
 }
+
+
