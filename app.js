723367ba5994bad8e15c2b57bf77bc3332b64308
JoseLLoredoHdz <lluisloredo558@gmail.com>
2025-08-15 00:29:12 -0600

    Creacion de About , FQ , Footer , creando el css y el js para el funcionamiento de carrusel y agregando imagenes gelatina y de logo tambien la descripciones y demas como donde nos encontramos checar todo el funcionamiento adecuado y crear mas secciones y arreglar la parte de ingles del nav

---
+++ app.js
@@ -0,0 +1,70 @@
+let imagenes = [
+    {
+        "url": "Imagenes/foto1.jpg",
+        "nombre": "Gelatina 1",
+        "descripcion": "Hecha con todo el amor para el comensal"
+
+    },
+    {
+        "url": "Imagenes/foto2.jpg",
+        "nombre": "Gelatina 2",
+        "descripcion": "Hecha con todo el amor para el comensal"
+
+    },
+    {
+        "url": "Imagenes/foto3.jpg",
+        "nombre": "Gelatina 3",
+        "descripcion": "Hecha con todo el amor para el comensal"
+
+    },
+]
+
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.<p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.<p>'
+        }
+    } 
+}
